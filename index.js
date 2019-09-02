550bfcefb1e783cf23baaa350ed27dad8baa3893
Alex Tilatti <alex.tilatti@gmail.com>
2019-09-01 23:14:51 -0400

    latest update for total volumes

--- index.js
+++ index.js
@@ -12,115 +12,103 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
-
-let res = [];
-
-function getApiData (list, type, desc)  {
-  
-  for(var i = 0; i < list.length; i++) {
-    list[i] = {
-      value: Number(list[i][0]),
-      volume: Number(list[i][1]),
-    };
+const getApiData = async url => {
+  const response = await axios.get(url).then(function (response) {
+  let asks = response.data.asks;
+  let bids = response.data.bids;
+  let arr = [];
+    for (i=0; i<asks.length; i++) {
+      for (j=0; j<bids.length; j++) {
+        if (i === j) {
+          arr.push(asks[i].concat(bids[j]));      
+      }
+    } 
   }
-  // Sort list just in case
-  list.sort(function(a, b) {
-    if (a.value > b.value) {
-      return 1;
-    }
-    else if (a.value < b.value) {
-      return -1;
-    }
-    else {
-      return 0;
-    }
-  });
+  console.log(arr);
+  return arr;
+});
 
-  // Calculate cummulative volume
-  if (desc) {
-    for(var i = list.length - 1; i >= 0; i--) {
-      if (i < (list.length - 1)) {
-        list[i].totalvolume = list[i+1].totalvolume + list[i].volume;
-      }
-      else {
-        list[i].totalvolume = list[i].volume;
-      }
-      var dp = {};
-      dp["value"] = list[i].value;
-      dp[type + "volume"] = list[i].volume;
-      dp[type + "totalvolume"] = list[i].totalvolume;
-      res.unshift(dp);
-    }
-  }
-  else {
-    for(var i = 0; i < list.length; i++) {
-      if (i > 0) {
-        list[i].totalvolume = list[i-1].totalvolume + list[i].volume;
-      }
-      else {
-        list[i].totalvolume = list[i].volume;
-      }
-      var dp = {};
-      dp["value"] = list[i].value;
-      dp[type + "volume"] = list[i].volume;
-      dp[type + "totalvolume"] = list[i].totalvolume; 
-      res.push(dp);
-    }
-  }
-  return res;
-}
+return response;
+};
 
 
 app.get("/", (req, res) => {
   res.render("orderbook");
 });
 
-app.get("/apiorder", (req, res) => {
-  console.log("hellooooo");
-  let firstUrl = "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_BTC&depth=10";
-  let secondUrl = "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDC_BTC&depth=10";
 
-let getFirstCoinData = async function() {
-  const firstCoinAsks = await axios.get(firstUrl).then(function (response) {
-    let getData = getApiData(response.data.asks, 'asks', false);
-    return getData;
+app.get("/orderbook", (req, res) => {
+  let firstUrl = "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_BTC&depth=50";
+  let secondUrl = "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDC_BTC&depth=50";
+
+  var getData = async function() {
+    const firstCoin = await getApiData(firstUrl);
+    const secondCoin = await getApiData(secondUrl);
+    let obj = {};
+    for (let i=0; i<firstCoin.length; i++) {
+      for (let j=0; j<secondCoin.length; j++) {
+        if (i === j) {
+          obj[i] = {  'FirstAskPrice' : Number(firstCoin[i][0]),
+                      'FirstAskVolume' : firstCoin[i][1],
+                      'FirstBidPrice' : Number(firstCoin[i][2]),
+                      'FirstBidVolume' : firstCoin[i][3],
+                      'SecondAskPrice' : Number(secondCoin[j][0]),
+                      'SecondAskVolume' : secondCoin[j][1],
+                      'SecondBidPrice' : Number(secondCoin[j][2]),
+                      'SecondBidVolume' : secondCoin[i][3]  };
+          // arr.push(usdt[i].concat(usdc[j]));
+        }
+      }
+    }
+    let newobj= Object.values(obj);
+    for (let i=0; i<newobj.length; i++) {
+      if (i > 0) {
+        obj[i].firstAskTotalVolume = obj[i-1].firstAskTotalVolume + obj[i].FirstAskVolume;
+        obj[i].secondAskTotalVolume = obj[i-1].secondAskTotalVolume + obj[i].SecondAskVolume;
+      } else {
+        obj[i].firstAskTotalVolume = obj[i].FirstAskVolume;
+        obj[i].secondAskTotalVolume = obj[i].SecondAskVolume;
+      }
+    }
+
+    for (let i = newobj.length - 1; i >= 0; i--) {
+      if (i < (newobj.length - 1)) {
+        obj[i].firstBidTotalVolume = obj[i+1].firstBidTotalVolume + obj[i].FirstBidVolume;
+        obj[i].secondBidTotalVolume = obj[i+1].secondBidTotalVolume + obj[i].SecondBidVolume;
+      }
+      else {
+        obj[i].firstBidTotalVolume = obj[i].FirstBidVolume;
+        obj[i].secondBidTotalVolume = obj[i].SecondBidVolume;
+      }
+    }            
+    console.log(newobj);
+    res.json(newobj);
+  };
+  getData();
 });
-  const firstCoinBids = await axios.get(firstUrl).then(function (response) {
-    let getData = getApiData(response.data.bids, "bids", true);
-    return getData;
-}); 
-  let processed = {'FIRSTCOIN' : [firstCoinAsks, firstCoinBids]};
-  return processed;
-};
 
-let getSecondCoinData = async function() {
-  const secondCoinAsks = await axios.get(secondUrl).then(function (response) {
-    let getData = getApiData(response.data.asks, 'asks', false);
-    return getData;
-});
-  const secondCoinBids = await axios.get(secondUrl).then(function (response) {
-    let getData = getApiData(response.data.bids, "bids", true);
-    return getData;
-}); 
-  let processed = {'SECONDCOIN' : [secondCoinAsks, secondCoinBids]};
-  return processed;
-};
+// {  'FirstAskPrice' : firstCoin[i][0],
+//                       'FirstAskVolume' : firstCoin[i][1],
+//                       'FirstBidPrice' : firstCoin[i][2],
+//                       'FirstBidVolume' : firstCoin[i][3],
+//                       'SecondAskPrice' : secondCoin[j][0],
+//                       'SecondAskVolume' : secondCoin[j][1],
+//                       'SecondBidPrice' : secondCoin[j][2],
+//                       'SecondBidVolume' : secondCoin[i][3],
+                      // 'firstBidTotalVolume' :,
+                      // 'secondBidTotalVolume' :,  
+                      // 'firstAskTotalVolume' :,
+                      // 'secondAskTotalVolume' : };
 
-// var getSecondData = async function() {
-//     const secondCoin = await axios.get(usdcUrl).then(function () {
-//       getApiData(response.asks, 'asks', false);
-//       getApiData(response.bids, "bids", true);
-//     });
-//   return secondCoin;
+
+// let sendData = async function () {
+//   let firstData = await getFirstCoinData();
+//   let secondData = await getSecondCoinData();
+//   res.json([firstData, secondData]);
+//   return;
 // };
-let sendData = async function () {
-  let firstData = await getFirstCoinData();
-  let secondData = await getSecondCoinData();
-  res.json([firstData, secondData]);
-  return;
-};
 
-sendData();
+// sendData();
 
   //   let obj = {};
   //   for (let i=0; i<usdt.length; i++) {
@@ -141,7 +129,7 @@ sendData();
   //   res.send(usdt);
   //  };
   // getData();
-});
+
 
 app.listen(port, function() {
   console.log("Runnning on " + port);
